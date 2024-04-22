1af4245405033ee746e8c82f70e9df467fa148bb
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-04-22 18:27:36 +0000

    day1 end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,24 @@ class BooksController < ApplicationController
   end
 
   def index
+    # new book部分
+    @book = Book.new
+    # User info部分
+    
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :opinion, :image)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,14 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = User.find(params[:id])
+    
   end
 
   def edit
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)  
   end
 end
