c1fcb5f9fa9d7f1eedbb9cfe870f948bfc9ee762
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-04-24 10:36:56 +0000

    after mentar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,12 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   
-  def get_image
-    if image.attached?
-      image
-    else
-      'no_image.jpg'
-    end
+  
+  def get_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
+    image.variant(resize_to_limit: [width, height]).processed
+  end 
 end
